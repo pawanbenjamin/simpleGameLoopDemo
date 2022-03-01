dde58ac77e0815bcd9a72b3e4c8c02631b5ee744
pawanbenjamin <62716484+pawanbenjamin@users.noreply.github.com>
2022-03-01 01:05:47 -0500

    intertial kermit

---
+++ app.js
@@ -0,0 +1,13 @@
+// * Here's our game object:
+
+let gameState = {
+  currentScore: 0,
+  goal: 10,
+  gameStatus: 'playing',
+}
+
+// * Write functions to manipulate gameState -> Maybe even a method?
+
+// * Put Stuff of the screen
+
+// * Make them work together
