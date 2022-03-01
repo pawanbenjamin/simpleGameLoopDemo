98a71f7bce71edc4b94f610c9336f8db9b31bcf8
pawanbenjamin <62716484+pawanbenjamin@users.noreply.github.com>
2022-03-01 12:57:19 -0500

    comments

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ grid.classList.add('grid')
 
 board.appendChild(grid)
 
+//Makes elements and pushes them into our grid
 for (let i = 0; i < 20; i++) {
   for (let j = 0; j < 20; j++) {
     const cell = document.createElement('div')
